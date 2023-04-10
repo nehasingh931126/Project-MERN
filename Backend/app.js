d444010a0f618be0c363e7084e5e70ccafd3350e
Neha Singh <neha.singh931126@gmail.com>
2023-04-10 14:57:19 +0200

    registered the users routes to the app.js

--- Backend/app.js
+++ Backend/app.js
@@ -3,14 +3,14 @@ const bodyParser = require('body-parser');
 const HttpError = require('./model/http-error');
 
 const placesRoutes = require('./routes/places-routes');
-
+const usersRoutes = require('./routes/user-routes');
 const app = express();
 
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 5000;
 
 app.use('/api/places',placesRoutes);
-
+app.use("/app/user", usersRoutes);
 
 // this will be called when there is no route matching
 app.use((req, res, next)=> {
