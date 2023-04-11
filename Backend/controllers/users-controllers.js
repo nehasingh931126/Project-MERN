59fba887ba5b61e3dc29358b089d1b59408f49cc
Neha Singh <neha.singh931126@gmail.com>
2023-04-11 19:50:58 +0200

    Adding the user api with mongoose: login, signup, getlist

--- Backend/controllers/users-controllers.js
+++ Backend/controllers/users-controllers.js
@@ -1,56 +1,92 @@
 const HttpError = require("../model/http-error");
-const {v4: uuid} = require('uuid');
-const UserModel = require('../model/users');
-const DUMMY_JSON = [{
-    id: 'u1',
-    name: 'ns',
-    email: 'ns.ps@gmail.com',
-    password: "test"
-}]
-const getUsers = (req, res, next) => {
-  return res.json({ users: DUMMY_JSON });
+const { v4: uuid } = require("uuid");
+const UserModel = require("../model/users");
+const {validationResult}  = require('express-validator');
+const DUMMY_JSON = [
+  {
+    id: "u1",
+    name: "ns",
+    email: "ns.ps@gmail.com",
+    password: "test",
+  },
+];
+const getUsers = async (req, res, next) => {
+  let userList;
+  try{
+    userList = await UserModel.find({}, '-password');
+  } catch(error) {
+    const errorObject = new HttpError('Could not fetch list, something went wrong', 500);
+    return next(errorObject);
+  }
+  
+  return res.json({ users: userList.map(user=>user.toObject({getters: true}))});
 };
 
 const signup = async (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      console.log(422);
-      throw new HttpError("Invalid input passed, please check your data", 422);
-    }
-    const {name, email, password } = req.body;
-    try {
-      const existingUser = await User.findOne({email: email});
-    } catch (error) {
-      return next(new HttpError('Singup failed, please try again later'))
-    }
-     
-    const newUser = {
-        id: uuid(),
-        name,
-        email,
-        password
-    }
-    DUMMY_JSON.push(newUser);
-    return res.status(201).json({ user: newUser });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(422);
+    return next(new HttpError("Invalid input passed, please check your data", 422));
+  }
+  const { name, email, password, places } = req.body;
+  let existingUser;
+  try {
+    existingUser = await UserModel.findOne({ email: email });
+  } catch (error) {
+    return next(new HttpError("Signup failed, please try again later"));
+  }
+  console.log(existingUser);
+  if (existingUser) {
+    return next(
+      new HttpError("User exists already, please login instead", 500)
+    );
+  }
+  let newUser;
+
+  try {
+    newUser = new UserModel({
+      name,
+      email,
+      password,
+      image: "https://image.com",
+      places,
+    });
+
+    await newUser.save();
+  } catch (error) {
+    console.log(error);
+    return next(
+      new HttpError("Signup failed, please try later", 422)
+    );
+  }
+
+  return res.status(201).json({ user: newUser.toObject({getters: true}) }); // this code of toObject removes the underscore from _id property
 };
 
-const login = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      console.log(422);
-      throw new HttpError("Invalid input passed, please check your data", 422);
-    }
-    const {email, password} = req.body;
-    const identifiedUser = DUMMY_JSON.find(user=> user.email === email);
-    if (!identifiedUser || identifiedUser.password !== password) {
-      throw new HttpError(
-        "Could not identify user, credentials seem to be wrong.",
-        401
-      );
-    }
-    res.json({message: "login successful"})
-};
+const login = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(422);
+    return next(new HttpError("Invalid input passed, please check your data", 422));
+  }
+  const { email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await UserModel.findOne({email});
+  }  catch (error) {
+    return next(new HttpError('Could not login, something when wrong', 500))
+  }
 
+  console.log(existingUser);
+
+  if (!existingUser || existingUser.password !== password) {
+    return next(new HttpError(
+      "Could not identify user, credentials seem to be wrong.",
+      401
+    ));
+  }
+  res.json({ message: "login successful" });
+};
 
 module.exports.getUsers = getUsers;
 module.exports.signup = signup;
--- Backend/model/users.js
+++ Backend/model/users.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
   name: { type: String, required: true },
-  email: { type: String, email: true, required: true, unique: true },
+  email: { type: String, index: true,required: true, unique: true },
   password: { type: String, required: true, minlength: 6 },
-  image: {type: String, required: true},
-  places: {type: String, required: true}
+  image: { type: String, required: true },
+  places: { type: String, required: true },
 });
 
-userSchema.plugin(uniqueValidator); // this is to validate only one email address is present in the database 
+// UserSchema.plugin(uniqueValidator); // this is to validate only one email address is present in the database 
 
 module.exports = mongoose.model('User', UserSchema);
 
